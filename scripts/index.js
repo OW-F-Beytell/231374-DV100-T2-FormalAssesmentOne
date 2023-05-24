849f05d4ef96755d73a74a84c1a36a4c40e8b9d3
OW-F-Beytell <125928514+OW-F-Beytell@users.noreply.github.com>
2023-05-24 03:26:37 +0200

    Functions
    Implement the JS functions
    Checkbox does not pass correctly

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+console.log("hello world!");
+
+generateAlert = () => {
+    let name = document.getElementById("inputName").value;
+    let mail = document.getElementById("inputEmail").value;
+    let subject = document.getElementById("inputSelect").value;
+    let number = document.getElementById("inputNumber").value;
+    let newsletter = document.getElementById("inputCheck").value;
+    alert(newsletter);
+    if (newsletter === 1) {
+        alert("Welcome " + name + "! Thank you for your interest in " + subject +"! We will contact you via the email provided: " + mail + ", or alternatively on the phone number: "+ number + ".");
+    }
+    else {
+        alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested" + name + "! If you want to sign up, you better CHECK that newsletter box! ");
+    }
+}
