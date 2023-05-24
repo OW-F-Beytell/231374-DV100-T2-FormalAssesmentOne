03463ea9d60dabcd8903509ef1b54f4a0fac9c2c
OW-F-Beytell <125928514+OW-F-Beytell@users.noreply.github.com>
2023-05-24 10:26:23 +0200

    Done
    Fix passing of checkbox value
    Test that everything works correctly

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,12 @@ generateAlert = () => {
     let mail = document.getElementById("inputEmail").value;
     let subject = document.getElementById("inputSelect").value;
     let number = document.getElementById("inputNumber").value;
-    let newsletter = document.getElementById("inputCheck").value;
-    alert(newsletter);
-    if (newsletter === 1) {
+    let newsletter = document.getElementById("inputCheckbox").checked;
+    
+    if (newsletter == 1) {
         alert("Welcome " + name + "! Thank you for your interest in " + subject +"! We will contact you via the email provided: " + mail + ", or alternatively on the phone number: "+ number + ".");
     }
     else {
-        alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested" + name + "! If you want to sign up, you better CHECK that newsletter box! ");
+        alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested " + name + "! If you want to sign up, you better CHECK that newsletter box! ");
     }
 }
